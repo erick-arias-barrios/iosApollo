ec2f5117bc2328dc7dc84ad40a89ff8b1165a5d6
Erick Arias <erick.arias.barrios@gmail.com>
2022-07-27 14:31:45 -0500

    Initial Commit

---
+++ iosApollo/iosApolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApolloApp.swift
+//  iosApollo
+//
+//  Created by Erick Arias on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct iosApolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
